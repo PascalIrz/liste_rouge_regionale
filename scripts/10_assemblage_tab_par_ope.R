1ac7d8b254bd7acf507af37cb820e93ffa821ffa
LeaBouch <bouchetlea@laposte.net>
2024-02-13 17:09:29 +0100

    Travail 13/02 - j'ai réussi à créer ope_selection_param_env3 contenant les NA mais des ope_id.X / ope_id.y et pop_id.x / pop_id.y sont apparus. Il s'agit de doublons, je vais essayer de rectifier demain !

--- scripts/10_assemblage_tab_par_ope.R
+++ scripts/10_assemblage_tab_par_ope.R
@@ -1,8 +1,17 @@
 ## Chargement des packages et des données ----
 
+
+#install.packages("khroma")
+#install.packages("lemon")
+#install.packages("ggthemes")
+
+library(lemon)
+library(ggthemes)
 library(tidyverse)
 library(aspe)
 library(ggplot2)
+library (khroma)
+
 
 load(file = "processed_data/selection_pop_ope.rda")
 
@@ -19,6 +28,9 @@ mei_table <- misc_nom_dernier_fichier(
 load(mei_table)
 
 source(file = "R/calcul_biomasse.R")
+
+
+
 
 
 ############################# DENSITES SURFACIQUES ###############################
@@ -76,7 +88,6 @@ ds_ope <- ope_selection_densite %>% #ds pour densité surfacique
          indicateur,
          valeur,
          -effectif)
-
 
 
 
@@ -160,23 +171,6 @@ l_75_ope <- l_75_ope %>%
 
 
 
-# J'empile les différentes valeurs pour créer le tableau pré-final
-indicateur_ope <- rbind(ds_ope,lm_ope,l_inter_qua_ope,l_25_ope,l_75_ope)
-
-#Vérification que nous avons bien la même valeur pour nos différents indicateurs
-table(indicateur_ope$indicateur)
-
-
-
-
-
-
-
-
-
-
-
-
 
 ########################### DONNEES ENVIRONNEMENTALES #########################
 
@@ -197,20 +191,107 @@ ope_selection_param_env <- passerelle %>%
 ope_selection_param_env2 <- ope_selection_param_env %>% 
   pivot_longer(altitude:temp_janvier,
                names_to = "parametre",
-               values_to= "valeur")
- 
+               values_to= "valeur") 
 
-# Représentation de mes variables environnements
 
-mes_id <- sample(unique(ope_selection_param_env2$pop_id), 5)
+# GESTION DES NA DANS LE JEU DE DONNEES ENV ---- 
+
+# Je fais apparaître mes NA dans mon jeu de données environnement : 
+
+
+ope_selection_param_env3 <- left_join(ope_selection_param_env, 
+                                      ope_selection_param_env2,
+                                      by = "annee")
+
+
+# Une fois que mes NA seront apparants : je remplace ces NA par les médianes :
+
+ope_selection_param_env3 %>% 
+  group_by(parametre, pop_id.x) %>% 
+  mutate (valeur = (ifelse (is.na (valeur), 
+                           yes = median (valeur, na.rm = TRUE), 
+                           no = valeur)))
+
+
+
+
+# Représentation de mes variables environnements (avec facet_wrap) : 
+
+mes_id <- sample(unique(ope_selection_param_env2$pop_id), 2)
 ope_selection_param_env2 %>% 
   filter(pop_id%in% mes_id) %>% 
   ggplot(aes(x=annee, y=valeur)) +
-  geom_bar(stat="identity") +
+  geom_bar(stat="identity", fill = "darkcyan") +
   facet_wrap(vars(pop_id, parametre),
              ncol = 8,
-               scales="free_y")
+             scales="free_y") +
+  labs(title = "Diagrammes en baton des variables environnementales", 
+       x = "Années", 
+       y = "Valeurs") +
+  theme_clean() +
+  theme(
+    axis.title.y = element_text (color = "#993333"),
+    axis.text.y.left = element_text (color = "#993333"))
   
+
+
+# Représentation de mes variables environnements (avec facet_grid) : mais problème d'échelle
+
+
+mes_id <- sample(unique(ope_selection_param_env2$pop_id), 2) 
+ope_selection_param_env2 %>% 
+  filter(pop_id%in% mes_id) %>% 
+  ggplot(mapping = aes(x = annee, y = valeur)) +
+  geom_bar(stat="identity", fill = "darkcyan") +
+  facet_rep_grid(pop_id ~ parametre, 
+             scales= "free_y", 
+             repeat.tick.labels = TRUE) +
+  labs(title = "Diagrammes en baton des variables environnementales", 
+       x = "Années", 
+       y = "Valeurs") +
+  theme_bw()
+
+
+
+
+
+############################ DENSITE VOLUMIQUE #################################
+
+# Je créer un tableau contenant à la fois les densités surfaciques et les profondeurs
+
+tab_ds <- ds_ope %>% 
+  select(ope_id,
+         valeur)
+
+tab_prof <- ope_selection_param_env3 %>%
+  filter(parametre == "profondeur") %>% 
+  select(ope_id.x,
+         valeur)
+
+
+# Ici je suis bloquée car je n'ai pas encore mes valeurs médianes qui remplaces les NA ... donc pas
+# la même longueur au niveau de mes colonnes ! 
+
+dv_ope <- tab_ds %>% 
+  mutate(valeur = valeur / tab_prof %>% 
+           select(valeur))
+
+
+
+
+
+
+
+
+
+######################## CREATION TABLEAU FINAL EMPILE #######################
+
+# J'empile les différentes valeurs pour créer le tableau pré-final
+indicateur_ope <- rbind(ds_ope,lm_ope,l_inter_qua_ope,l_25_ope,l_75_ope)
+
+#Vérification que nous avons bien la même valeur pour nos différents indicateurs
+table(indicateur_ope$indicateur)
+
 
 
 
