b0cd103bc451a0ad79f8cadf2c60df2c971d3bb7
LeaBouch <bouchetlea@laposte.net>
2024-03-26 17:06:23 +0100

    MAJ

--- R/calcul_taux_evolution.R
+++ R/calcul_taux_evolution.R
@@ -10,45 +10,36 @@
 #' \dontrun{
 #' }
 
-calcul_moyenne_geom <- function(data, 
-                                var_x, 
-                                var_y,
-                                var_annee_depart,
-                                ...) 
+calcul_taux_evol <- function(df,
+                             var_x,
+                             var_y,
+                             debut,
+                             ...) 
 
   
 {
   var_x <- enquo(var_x)
   var_y <- enquo(var_y)
-  var_annee_depart <- enquo(var_annee_depart)
   vars_group <- quos(...)
 
   
   # Filtrer les données
-  df <- data %>% 
+  df1 <- df %>%
     filter(!is.na(!!var_y),
-           !!var_x >= !!var_annee_depart,
-           !!!vars_group)
+           !!var_x >= debut)
   
   # Calculer les différences annuelles et les taux annuels
-  df1 <- df %>% 
-    group_by(!!!vars_group) %>% 
-    arrange(!!var_x) %>% 
+  df2 <- df1 %>%
+    group_by(!!!vars_group) %>%
+    arrange(!!var_x) %>%
     mutate(diff_annee =  !!var_x  - lag(!!var_x , default = first( !!var_x )),
-           taux_annuel = ifelse(diff_annee > 0, (!!var_y / lag( !!var_y , default = first( !!var_y ))) ^ (1 / diff_annee), NA_real_)) %>% 
-    filter(!is.na(taux_annuel))
+           taux_annuel = ifelse(diff_annee > 0, (!!var_y / lag( !!var_y , default = first( !!var_y ))) ^ (1 / diff_annee), NA_real_)) %>%
+  filter(!is.na(taux_annuel))
   
-  # Calculer la moyenne géométrique des taux annuels
-  mean_geom <- df1 %>%
-    summarize(mean_geom = exp(mean(log(taux_annuel), na.rm = TRUE))) %>%
-    pull(mean_geom)
-  
-  # Créer un dataframe avec les valeurs calculées
-  result_df <- data.frame(Espece = unique(data$esp_code_alternatif),
-                          Indicateur = unique(data$indicateur),
-                          Annee_Depart = min(data[[!!var_x]]),
-                          Moyenne_Geometrique = mean_geom,
-                          Stade = unique(data$stade))
-  
-  return(result_df)
+ # Calculer la moyenne géométrique des taux annuels
+  mean_geom <- df2 %>%
+  summarize(mean_geom = exp(mean(log(taux_annuel)))) %>% 
+    mutate (debut = debut)
+
+  return(mean_geom)
 }
