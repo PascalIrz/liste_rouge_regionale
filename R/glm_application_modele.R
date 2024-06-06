11b545ff4a90d11e994313945e341574ec302186
LeaBouch <bouchetlea@laposte.net>
2024-06-06 15:06:36 +0200

    Mise à jour 06/06

---
+++ R/glm_application_modele.R
@@ -0,0 +1,40 @@
+glm_application_modele <- function(data, 
+                                   liste_periodes) {
+  
+  results_list <- map(liste_periodes, function(period) {
+    mon_annee_depart <- period[1]
+    mon_annee_fin <- period[2]
+    
+    # Filtrer les données pour la période courante
+    period_data <- data %>%
+      filter(annee >= mon_annee_depart & annee <= mon_annee_fin)
+    
+    combinations <- period_data %>%
+      select(espece, 
+             stade, 
+             indicateur) %>%
+      distinct()
+    
+    period_label <- paste0(mon_annee_depart, "_", mon_annee_fin)
+    
+    combinations %>%
+      pmap(function(espece, 
+                    stade, 
+                    indicateur) {
+        
+        results <- glm_calcul_modele(data = period_data, 
+                                     mon_espece = espece, 
+                                     mon_stade = stade, 
+                                     mon_indicateur = indicateur)
+        if (!is.null(results)) {
+          results <- results %>%
+            mutate(periode = period_label)
+        }
+        return(results)
+      })
+  }) %>% 
+    flatten() %>%
+    bind_rows()
+  
+  return(results_list)
+}
--- R/glm_calcul_modele.R
+++ R/glm_calcul_modele.R
@@ -1,30 +1,28 @@
-
-calculate_model_glmm <- function(data, 
-                                 mon_espece, 
-                                 mon_stade, 
-                                 mon_indicateur) {
+glm_calcul_modele <- function(data,
+                              mon_espece,
+                              mon_stade,
+                              mon_indicateur) {
   
   filtered_data <- data %>%
     filter(espece == mon_espece, 
            stade == mon_stade, 
            indicateur == mon_indicateur)
   
-  # Vérifiez si les données sont suffisantes pour ajuster le modèle
   if (nrow(filtered_data) < 2 || 
       length(unique(filtered_data$pop_id)) < 2 || 
       length(unique(filtered_data$annee)) < 2 || 
       length(unique(filtered_data$pro_libelle)) < 2 || 
       length(unique(filtered_data$obj_libelle)) < 2) {
-    return(NULL)  # Retournez NULL si les données sont insuffisantes
+    return(NULL)
   }
   
   model <- try(lmer(valeur ~ (1 | pop_id) + annee + pro_libelle + obj_libelle, data = filtered_data), silent = TRUE)
   
   if (inherits(model, "try-error")) {
-    return(NULL)  # Retournez NULL si le modèle échoue
+    return(NULL)
   }
   
-  res <- summary(model)$coefficients %>%  # Obtenir les résultats résumés
+  res <- summary(model)$coefficients %>%
     as.data.frame() %>%
     rename(p_value = `Pr(>|t|)`) %>%
     mutate(sig = case_when(
